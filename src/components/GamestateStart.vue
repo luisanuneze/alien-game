1816ab2e6233a37db295e2e38fc3bf35f253f808
Luisa <luisa.nunez150@gmail.com>
2021-05-18 08:50:29 -0600

    first commit

---
+++ src/components/GamestateStart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="modal">
+    <div>
+      <h1 id="header">You're an alienzombie.</h1>
+      <p>
+        In order to survive on Earth, you have to pretend to be a human on this
+        planet. You took over the body of a nearby hooman, but the trouble is,
+        their best friend showed up!
+      </p>
+      <p>
+        They look suspicious. Answer the questions right to make sure identity
+        is kept secret!
+      </p>
+      <slot> </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
